0d035a4f25d159ddda3db00cc12a3b7d6e64bf05
Tim Davidson <tdavidson@ACC0291.local>
2018-01-31 19:49:27 -0400

    Cleanup

--- js/canvas/images.js
+++ js/canvas/images.js
@@ -51,38 +51,3 @@ function fetchImageFor(src) {
   //console.log('*** IMG loading: ' + kImagePath + src + '.png');
   return null;
 }
-
-
-/*
-var sources = {
-  bg: 'templates/bb.png',
-  bgNeutral: 'templates/bg_neutral.png',
-  border: 'templates/border.png',
-  artBorder: 'templates/border_art.png',
-  verbTitle: 'templates/title_verb.png',
-  verbText: 'templates/text_verb.png'
-};
-function loadImages(sources) {  // , callback) {
-  var loadedImages = 0;
-  var numImages = 0;
-  // get num of sources
-  for (var src in sources) {
-    numImages++;
-  }
-  for (var src in sources) {
-    images[src] = new Image();
-
-    images[src].onload = function () {
-      if (++loadedImages >= numImages) {
-        redraw(can);
-      }
-    };
-
-    // fixed 'tainted' error when saving, but moced CORS issue to here.
-    //images[src].crossOrigin = 'Anonymous';
-
-    images[src].src = kImagePath + sources[src];
-  }
-}
-loadImages(sources);
-*/
